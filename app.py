d376a36eaa05f659eef1f6ced1fa8026e4440bc4
Anthony Ryan <aryan@masseyservices.com>
2020-10-02 16:35:28 -0400

    added password hashing

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from config import Config
 from extensions import db
-from models.user import User
+from resources.user import UserListResource
 from resources.recipe import RecipeListResource, RecipeResource, RecipePublishResource
 
 def create_app():
@@ -19,6 +19,7 @@ def register_extensions(app):
 
 def register_resources(app):
     api = Api(app)
+    api.add_resource(UserListResource,'/users')
     api.add_resource(RecipeListResource, '/recipes')
     api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
     api.add_resource(RecipePublishResource, '/recipes/<int:recipe_id>/publish')
