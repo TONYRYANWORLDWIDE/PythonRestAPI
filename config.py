abe0ae359de5d3267d1fa7496e2d0d2163c63676
Anthony Ryan <aryan@masseyservices.com>
2020-10-02 15:09:36 -0400

    got migrations working

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://tonyryan:prolong-vivacity-asbestos@localhost/smilecook'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
